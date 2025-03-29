d98a991de5bc624d03e76e6f0ce7d8b58717f0cc
Arjun Chandra <ac25@bu.edu>
2025-03-28 23:24:49 -0400

    Added new models and absolute filepath

--- benchmark/code/models.py
+++ benchmark/code/models.py
@@ -4,7 +4,7 @@ from open_clip import create_model_from_pretrained, get_tokenizer
 
 def get_DAC_SAM(device):
 
-    CHECKPOINT_PATH = "checkpoints/sam_cp.pt"
+    CHECKPOINT_PATH = "/projectnb/cs598/projects/comp_reason/CS598-VLC/benchmark/checkpoints/sam_cp.pt"
 
     model, preprocess_train, preprocess_val = create_model_and_transforms(
         "ViT-B/32",
@@ -34,7 +34,53 @@ def get_DAC_SAM(device):
         image_features, text_features, logit_scale = model(images, text)
         return image_features, text_features
 
-    return model_forward, preprocess_train, tokenize
+    return model_forward, preprocess_train, preprocess_val, tokenize
+
+def get_DAC_SAM_base(device):
+
+    model, preprocess_train, preprocess_val = create_model_and_transforms(
+        "ViT-B/32",       
+        "openai",    #can also change this here to load from open clip library 
+        precision="amp",  
+        device=device,    
+        jit=False,      
+        force_quick_gelu=False,
+        image_mean=None,
+        image_std=None,
+    )
+
+    model.eval()
+
+    # Define the model's forward pass function
+    def model_forward(images, text):
+        image_features, text_features, logit_scale = model(images, text)
+        return image_features, text_features
+
+    # Return the forward pass function and preprocessing functions
+    return model_forward, preprocess_train, preprocess_val, tokenize
+
+def get_DAC_SAM_base_2(device):
+    model, preprocess_train, preprocess_val = create_model_and_transforms(
+        "ViT-B/32",       
+        "laion400m_e32",    #loading same base model but different pretraining data
+        precision="amp",  
+        device=device,    
+        jit=False,      
+        force_quick_gelu=False,
+        image_mean=None,
+        image_std=None,
+    )
+
+    model.eval()
+
+    # Define the model's forward pass function
+    def model_forward(images, text):
+        image_features, text_features, logit_scale = model(images, text)
+        return image_features, text_features
+
+    # Return the forward pass function and preprocessing functions
+    return model_forward, preprocess_train, preprocess_val, tokenize
+
 
 def get_ViT(device):
     model, preprocess = create_model_from_pretrained('hf-hub:timm/ViT-L-16-SigLIP-256')
@@ -48,10 +94,12 @@ def get_ViT(device):
         image_features = torch.nn.functional.normalize(image_features, dim=-1)
         text_features = torch.nn.functional.normalize(text_features, dim=-1)
         return image_features, text_features
-    return model_forward, preprocess, tokenizer_forward
+    return model_forward, preprocess, preprocess, tokenizer_forward
 
 MODELS = {
     "DAC-SAM": get_DAC_SAM,
+    "DAC-SAM-base": get_DAC_SAM_base,
+    "DAC-SAM-base-2": get_DAC_SAM_base_2,
     "ViT": get_ViT,
 }
 
